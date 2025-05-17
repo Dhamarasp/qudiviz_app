316ccaee5820c705573c2d6fd4bfbb67f70efcad
dhamarasp <dhamarsusyatama22@gmail.com>
2025-05-17 15:00:44 +0700

    URL Force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Load helpers
         require_once app_path('Helpers/ToastHelper.php');
     }
