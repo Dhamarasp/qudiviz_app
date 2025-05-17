4d3f12fc25d2186eefad1304504d5cd1be41536c
dhamarasp <dhamarsusyatama22@gmail.com>
2025-05-17 15:06:48 +0700

    Toast Helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Load helpers
+        // Load helpers Toast
         require_once app_path('Helpers/ToastHelper.php');
     }
 }
